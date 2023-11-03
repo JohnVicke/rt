c67c51c99080a7148163c212311ed78030826393
ViktorM <viktor@malmedal.dev>
2023-11-03 17:32:19 +0100

    update routes

--- packages/server/src/__tests__/core/route.test.ts
+++ packages/server/src/__tests__/core/route.test.ts
@@ -1,11 +1,71 @@
 import { object, string } from "valibot";
 import { RTRoute } from "../../core/route";
-import { describe, it } from "bun:test";
+import { describe, expect, it } from "bun:test";
 import { Equal, Expect } from "../__type-helpers";
 
 const testInputSchema = object({ name: string(), test: string() });
 
 describe("RTRoute", () => {
+  describe("procedure flow", () => {
+    it("should verify body and return correct response", async () => {
+      const route = new RTRoute();
+      route.body(testInputSchema).handler(({ body }) => {
+        return new Response(JSON.stringify(body));
+      });
+
+      const testUrl = new URL("http://localhost:3000/test");
+      const testRequest = new Request(testUrl.toString(), {
+        body: JSON.stringify({ name: "test", test: "test" }),
+      });
+
+      const procedure = route.procedeur();
+      const response = await procedure(testUrl, testRequest);
+      expect(response.status).toBe(200);
+      const json = await response.json();
+      expect(json).toEqual({ name: "test", test: "test" });
+    });
+    it("should throw 400 error if body is incorrect", async () => {
+      const route = new RTRoute();
+      route.body(testInputSchema).handler(({ body }) => {
+        return new Response(JSON.stringify(body));
+      });
+
+      const testUrl = new URL("http://localhost:3000/test");
+      const testRequest = new Request(testUrl.toString(), {
+        body: JSON.stringify({ foo: "test" }),
+      });
+
+      const procedure = route.procedeur();
+      const response = await procedure(testUrl, testRequest);
+      expect(response.status).toBe(400);
+    });
+    it("should verify params and return correct response", async () => {
+      const route = new RTRoute();
+      route.params(testInputSchema).handler(({ params }) => {
+        return new Response(JSON.stringify(params));
+      });
+
+      const testUrl = new URL("http://localhost:3000?name=test&test=test");
+      const testRequest = new Request(testUrl.toString());
+
+      const procedure = route.procedeur();
+      const response = await procedure(testUrl, testRequest);
+      expect(response.status).toBe(200);
+    });
+    it("should throw 400 error if params are incorrect", async () => {
+      const route = new RTRoute();
+      route.params(testInputSchema).handler(({ params }) => {
+        return new Response(JSON.stringify(params));
+      });
+
+      const testUrl = new URL("http://localhost:3000");
+      const testRequest = new Request(testUrl.toString());
+
+      const procedure = route.procedeur();
+      const response = await procedure(testUrl, testRequest);
+      expect(response.status).toBe(400);
+    });
+  });
   describe("[TYPES]", () => {
     it("should infer body schema in handler if provided", () => {
       new RTRoute().body(testInputSchema).handler(({ body }) => {
--- packages/server/src/core/route.ts
+++ packages/server/src/core/route.ts
@@ -1,4 +1,5 @@
-import { type Input, type BaseSchema } from "valibot";
+import { type Input, type BaseSchema, parseAsync, ValiError } from "valibot";
+import { HttpStatusCode } from "../types/http-code";
 
 type HandlerFnOptions<TBody, TQuery> = {
   body: TBody extends BaseSchema ? Input<TBody> : never;
@@ -28,4 +29,69 @@ export class RTRoute<TBody = unknown, TParams = unknown> {
     this.handlerFn = handlerFn;
     return this;
   }
+
+  private async bodyPromise(req: Request) {
+    if (!req.body || !this.bodySchema) {
+      return Promise.resolve(undefined);
+    }
+
+    const json = await req.json();
+
+    return parseAsync(this.bodySchema, json);
+  }
+
+  private async paramPromise(url: URL) {
+    if (!this.paramSchema) {
+      return Promise.resolve(undefined);
+    }
+
+    return parseAsync(this.paramSchema, Object.fromEntries(url.searchParams));
+  }
+
+  procedeur() {
+    return async (url: URL, req: Request) => {
+      if (!this.handlerFn) {
+        throw new Error("Handler is not defined");
+      }
+
+      const bodyPromise = this.bodyPromise(req);
+      const paramsPromise = this.paramPromise(url);
+
+      const [body, params] = await Promise.allSettled([
+        bodyPromise,
+        paramsPromise,
+      ]);
+
+      if (body.status === "rejected") {
+        return new Response(getErrorMsg(body.reason, "invalid body"), {
+          status: HttpStatusCode.BAD_REQUEST,
+        });
+      }
+
+      if (params.status === "rejected") {
+        return new Response(getErrorMsg(params.reason, "invalid body"), {
+          status: HttpStatusCode.BAD_REQUEST,
+        });
+      }
+
+      const options = {
+        body: body.value,
+        params: params.value,
+      } as HandlerFnOptions<TBody, TParams>;
+
+      return this.handlerFn(options);
+    };
+  }
 }
+
+const getErrorMsg = (error: any, fallback: string) => {
+  if (error instanceof ValiError) {
+    return JSON.stringify(
+      error.issues.map((i) => ({
+        message: i.message,
+        path: i.path?.map((p) => p.key).join("."),
+      })),
+    );
+  }
+  return fallback;
+};
