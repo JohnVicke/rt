ead5cd46f8e5cecbf4549a483263f28cf3edfbe8
Viktor Malmedal <viktor@malmedal.dev>
2023-11-06 21:42:11 +0100

    add test for client

--- packages/client/test/core/rt-client.test.ts
+++ packages/client/test/core/rt-client.test.ts
@@ -7,12 +7,28 @@ import { object, string } from "valibot";
 describe("RTClient", () => {
   const app = new RT()
     .get("/", () => "hello world")
-    .post("/hello", { body: object({ name: string() }) }, async (ctx) => {
+    .post("/hello", { body: object({ name: string() }) }, (ctx) => {
       return { name: ctx.body.name };
+    })
+    .post("/test", (ctx) => {
+      return "no body";
     });
 
   it("infers return type for get", async () => {
     const client = new RTClient<typeof app>("http://localhost:3000");
     expectTypeOf(client.get("/")).toEqualTypeOf<Promise<string>>();
+  });
+
+  it("infers return type for post", async () => {
+    const client = new RTClient<typeof app>("http://localhost:3000");
+    const response = client.post("/hello", { name: "hello" });
+
+    expectTypeOf(client.post("/hello", { name: "hello" })).toEqualTypeOf<
+      Promise<{ name: string }>
+    >();
+  });
+  it("infers return type for post with no body", async () => {
+    const client = new RTClient<typeof app>("http://localhost:3000");
+    expectTypeOf(client.post("/test")).toEqualTypeOf<Promise<string>>();
   });
 });
