9a87dc7ec0b7e70b0824049ae8ed383be20b2787
Viktor Malmedal <viktor@malmedal.dev>
2023-11-06 21:41:30 +0100

    feat: add minimal example

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello world");
+import { RT } from "@rt/server";
+
+new RT()
+  .get("/", () => "Hello World!")
+  .get("/hello", () => ({ message: "Hello World!" }))
+  .listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
